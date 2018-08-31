179d249f3931d53125872601c98ca68b631a7c2f
AReid987 <read.musik@gmail.com>
2018-08-31 17:16:56 -0500

    created stub for NotesPage container

---
+++ client/src/containers/NotesPage.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { fetchNotes } from '../actions/noteActions';
+
+class NotesPage extends Component {
+
+  render() {
+    return (
+      <div>
+      </div>
+    )
+  }
+}
+
+export default NotesPage
