0e4ed769ef6bda8a1648fc8d2b1e90825a122a67
AReid987 <read.musik@gmail.com>
2018-09-02 15:45:36 -0500

    LOAD_NOTE case for notesReducer

--- client/src/reducers/notesReducer.js
+++ client/src/reducers/notesReducer.js
@@ -10,6 +10,12 @@ export default function notesReducer(state = {
     case 'LOAD_ALL_NOTES':
       return { loading: false, notes: action.payload }
 
+    case 'LOAD_NOTE':
+      return [
+        ...state.filter(note => note.id !== action.payload.id),
+        Object.assign({}, action.payload)
+      ]
+
     default:
       return state;
   }
