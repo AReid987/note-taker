211687da4549bfa4850136530c2d034be8795c8e
AReid987 <read.musik@gmail.com>
2018-09-01 22:58:57 -0500

    added edit button and event handler to NoteShow

--- client/src/containers/NoteShow.js
+++ client/src/containers/NoteShow.js
@@ -2,11 +2,17 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
 class NoteShow extends Component {
-  constructor(props, context){
-    super(props, context);
+  constructor(props){
+    super(props);
     this.state = {
       isEditing: false
     }
+  }
+
+  handleOnClick = event => {
+    this.setState({
+      isEditing: !this.state.isEditing
+    })
   }
 
   render(){
@@ -15,6 +21,7 @@ class NoteShow extends Component {
       <div>
         <h2>{this.props.note.title}</h2>
         <p>{this.props.note.content}</p>
+        <button onClick={this.handleOnClick}>Edit</button>
       </div>
     )
   }
