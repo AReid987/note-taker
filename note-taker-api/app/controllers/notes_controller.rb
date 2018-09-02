679f626b60e8f0f7749eeff6f47a6b8d6669f7f7
AReid987 <read.musik@gmail.com>
2018-09-02 14:51:39 -0500

    changed routes in rails to allow for all resources, added stub for update and added show action in NotesController.rb

--- note-taker-api/app/controllers/notes_controller.rb
+++ note-taker-api/app/controllers/notes_controller.rb
@@ -5,6 +5,13 @@ class NotesController < ApplicationController
     render json: @notes
   end
 
+  def show
+    @note = Note.find(params[:id])
+    render json: @note
+  end
+
+  def update
+  end 
   # private
   #
   # def note_params
